a69274a64d197c42dc0f7197e216631604c9091e
zeta2104 <giulianozuppone@gmail.com>
2024-10-31 23:42:32 -0300

    Creacion clase Almacenamiento
    se creo la clase almacenamiento y se le agregaron 3 metodos para el arraylist generico ( eliminar agregar y mostrar)

---
+++ TP FINAL/src/PackageContenedores/AlmacenamientoNPC.java
@@ -0,0 +1,51 @@
+package PackageContenedores;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class AlmacenamientoNPC<T> {
+    //todo.ATRIBUTO
+    protected ArrayList<T> arrayNPC;
+
+    //todo.CONSTRUCTOR
+
+    public AlmacenamientoNPC() {
+        this.arrayNPC = new ArrayList<>();
+    }
+
+    //todo.METODOS
+
+    public String agregar(T NPC){
+        String mensaje="";
+        if(NPC != null){
+            arrayNPC.add(NPC);
+            mensaje="Se agrego el npc correctamente";
+        }else{
+            mensaje="No ha sido posible su carga.";
+        }
+        return mensaje;
+    }
+
+
+    public String eliminar(T NPC){
+        String mensaje="";
+        if(NPC == null){
+            mensaje = "No se puede eliminar porq es null a ver si te das cuenta tontito";
+        }else{
+            arrayNPC.remove(NPC);
+            mensaje = "Se elimino el objeto anashi";
+        }
+        return mensaje;
+    }
+
+    public String mostrarObjetos(){
+        StringBuilder sb = new StringBuilder();
+        Iterator<T> iterator = arrayNPC.iterator();
+
+        while(iterator.hasNext()){
+            sb.append(iterator.next().toString()).append("\n");
+        }
+        return sb.toString();
+    }
+
+}
